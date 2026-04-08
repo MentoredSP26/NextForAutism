2b6891b787a38fe604deeaa76269d5fd664e441e
nsaadhvi <narayanansaadhvi@gmail.com>
2026-04-08 00:16:52 -0700

    fixed page.js formatting

--- matchingplatform/src/app/page.js
+++ matchingplatform/src/app/page.js
@@ -1,7 +1,6 @@
 "use client"
 import Image from "next/image";
 import styles from "./page.module.css";
-<<<<<<< HEAD
 import App from "./admin/page";
 
 export default function Home() {
@@ -10,16 +9,5 @@ export default function Home() {
       <App />
     </div>
 
-=======
-import TodoList from "@/components/todo section/page";
-import RecentActivity from "@/components/Admin Recent Activities/page";
-
-export default function Home() {
-  return (
-    <p>
-      <RecentActivity />
-    </p>
-    
->>>>>>> 1d27642 (todo done)
   );
 }
