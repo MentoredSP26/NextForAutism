1e2fd9022f0b47304805680697de711584ec7333
nsaadhvi <narayanansaadhvi@gmail.com>
2026-04-08 11:45:33 -0700

    graceful handle null path for now

--- matchingplatform/src/components/NavButton/page.jsx
+++ matchingplatform/src/components/NavButton/page.jsx
@@ -4,10 +4,20 @@ import { useRouter } from "next/navigation";
 
 function NavButton(props) {
     const router=useRouter();
+
+    const handleClick = () => {
+        if (!props.path) {
+            console.log('No path');
+            return;
+        } else {
+            router.push(props.path)
+        }
+
+    }
     return (
         <div>
             {/* <Image alt="button icon"></Image> */}
-            <button onClick={() => router.push(props.path)}>{props.text}</button>
+            <button onClick={handleClick}>{props.text}</button>
         </div>
     )
 }
