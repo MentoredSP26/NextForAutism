84001e7874f52097dcf749bf24e633ae1e51391d
nsaadhvi <narayanansaadhvi@gmail.com>
2026-04-08 00:16:52 -0700

    fixed page formatting

--- matchingplatform/src/app/page.js
+++ matchingplatform/src/app/page.js
@@ -1,8 +1,7 @@
 "use client"
 import Image from "next/image";
 import styles from "./page.module.css";
-import TodoList from "@/components/todo section/page";
-import RecentActivity from "@/components/Admin Recent Activities/page";
+import TodoList from "@/components/TodoList/page";
 
 export default function Home() {
   return (
