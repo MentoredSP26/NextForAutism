e9f44a8bfcf7482c43cb1f4ddd28677a01f3eaef
Arianne de Guzman <ariannedeguzman@berkeley.edu>
2026-04-13 07:14:02 -0700

    finished navbar styling

--- matchingplatform/src/app/page.js
+++ matchingplatform/src/app/page.js
@@ -1,19 +1,19 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import NavBar from '../components/NavBar/page';
+import NavBar from '../components/navbar/page';
+
 
 export default function Home() {
   const buttons = [
-            {text: 'Dashboard',
-            path:null},
-            {text: 'Profile',
-            path:null}  
-        ]
+    { page: 'Dashboard', path: '/dashboard', icon: "home.png" },
+    { page: 'Profile', path: '/profile', icon: 'profile.png' },
+  ];
   return (<div>
-    <NavBar buttons={buttons}/>
-      <p>
+    <NavBar buttons={buttons} profile='Aspiring' user='user' email='email'/>
+      {/* <p>
         This is the home page (Next 4 Autism).
-      </p>
+      </p> */}
     </div>
+    
   );
 }
--- matchingplatform/src/components/navbar/page.jsx
+++ matchingplatform/src/components/navbar/page.jsx
@@ -4,26 +4,32 @@ import './styles.css';
 function NavBar(props) {
     const buttons = props.buttons;
     const profile = props.profile;
+    
     return (
         <div className="navBar">
+            
             <div className="nav-top">
-                <img className="nav-logo" alt="Next for Autism Logo" src="matchingplatform/public/next-for-autism-logo.svg"/>
-                <h1 className="nav-profile">{profile} Portal</h1>
+                <img className="logo" alt="Next for Autism Logo" src="logo.png"/>
+                <span className="nav-profile">{profile} Portal</span>
             </div>
             <nav className="navBar-buttons">
-                {buttons.map((btn, index) => (
-                    <NavButton key={index} text={btn.text} path={btn.path}/>
-                ))}
+                <ul>
+                    {buttons.map((btn, index) => (
+                        <NavButton key={index} page={btn.page} path={btn.path} icon={btn.icon}/>
+                    ))}
+                </ul>
             </nav>
             <div className="navBar-bottom">
-                <div className="nav-profile-circle">
-                    {/* <Image alt="Profile Circle"></Image> */}
+                <div className="bottom-content">
+                    <div className="nav-profile-circle">
+                        {/* <Image alt="Profile Circle"></Image> */}
+                    </div>
+                    <div className="user-text">
+                        <span className="user-name">{props.user}</span>
+                        <span className="user-email">{props.email}</span>
+                    </div>
                 </div>
-                <div>
-                <h1 className="user-name">{props.user}</h1>
-                <h3 className="user-email">{props.email}</h3>
-                </div>
-            </div>    
+            </div>
         </div>
     )
 }
