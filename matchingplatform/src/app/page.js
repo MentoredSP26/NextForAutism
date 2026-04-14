c8d4f1e616d4fc73d76de4ad9c418d999c2809b7
Arianne de Guzman <ariannedeguzman@berkeley.edu>
2026-04-13 22:54:09 -0700

    finished navbar styling

--- matchingplatform/src/app/page.js
+++ matchingplatform/src/app/page.js
@@ -1,19 +1,19 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import NavBar from '../components/navbar/page';
-
+import AdminHeader from '../components/adminHeader/page';
 
 export default function Home() {
   const buttons = [
     { page: 'Dashboard', path: '/dashboard', icon: "home.png" },
     { page: 'Profile', path: '/profile', icon: 'profile.png' },
   ];
-  return (<div>
-    <NavBar buttons={buttons} profile='Aspiring' user='user' email='email'/>
+  return (
+    <div>
+      <NavBar buttons={buttons} profile='Aspiring' user='user' email='email'/>
       {/* <p>
         This is the home page (Next 4 Autism).
       </p> */}
     </div>
-    
   );
 }
--- matchingplatform/src/components/adminHeader/page.jsx
+++ matchingplatform/src/components/adminHeader/page.jsx
@@ -1,24 +1,28 @@
-function adminHeader() {
+import './styles.css';
+
+function AdminHeader({name, position, bio}) {
     return(
-        <div className="adminHeaderCard">
-            <div className="adminHeaderContent">
-                {/* <Image alt="Admin Profile Picture"></Image> */}
-                <div className="adminTextContent">
-                    <h1 className="adminName">{props.name}</h1>
-                    <div className="adminBadge">
-                        <h3 className="adminPosition">{props.position}</h3>
+        <div className="admin-header-card">
+            <div className="inner-card">
+                <div className="admin-content">
+                    {/* <Image alt="Admin Profile Picture"></Image> */}
+                    <div className="admin-text-content">
+                        <span className="admin-name">{name}</span>
+                        <div className="admin-badge">
+                            <span className="admin-position">{position}</span>
+                        </div>
+                        <span className="admin-bio">{bio}</span>
                     </div>
-                    <p className="adminBio">{props.bio}</p>
                 </div>
-            </div>
-            <div className="adminEditProfile">
-                <button className="adminEditButton">
-                    {/* <Image alt="Edit Icon"></Image> */}
-                    Edit Profile
-                </button>
+                <div className="admin-edit-profile">
+                    <button className="admin-edit-button">
+                        {/* <Image alt="Edit Icon"></Image> */}
+                        Edit Profile
+                    </button>
+                </div>
             </div>
         </div>
     )
 }
 
-export default adminHeader
+export default AdminHeader
