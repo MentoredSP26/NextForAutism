9ad7ba7a77388e2d579739d7e08112eb9076312f
christian <christiant_07@berkeley.edu>
2026-04-13 22:58:19 -0700

    added some reminder comments for myself

--- matchingplatform/src/components/WeeklyMaterials/WeeklyMaterials.jsx
+++ matchingplatform/src/components/WeeklyMaterials/WeeklyMaterials.jsx
@@ -3,6 +3,10 @@ import './styles.css'
 import LearningBox from './LearningBox'
 import DiscussionBox from './DiscussionBox'
 
+// change font based on figma
+    //use google fonts, NFA styling doc, and color drop if neccesary
+    //some bolder,git  some lighter, sizing
+
 const weeks = [
     {   
         week: 1,
