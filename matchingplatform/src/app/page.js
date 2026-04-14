59d43d77ccde70577c2acff621d090cd960ef181
Jessica <lyuzimo@berkeley.edu>
2026-04-13 19:51:54 -0700

    recent activity

--- matchingplatform/src/app/page.js
+++ matchingplatform/src/app/page.js
@@ -2,11 +2,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import TodoList from "@/components/todo section/page";
+import RecentActivity from "@/components/Admin Recent Activities/page";
 
 export default function Home() {
   return (
     <p>
-      <TodoList />
+      <RecentActivity />
     </p>
     
   );
