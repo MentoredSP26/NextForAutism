78783ec7ee16fc777e2b2f70e4f033ffbd98637c
Jessica <lyuzimo@berkeley.edu>
2026-04-15 22:53:48 -0700

    fix styling

--- matchingplatform/src/app/page.js
+++ matchingplatform/src/app/page.js
@@ -2,12 +2,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import TodoList from "@/components/TodoList/page";
+import RecentActivity from "@/components/RecentActivity/page";
 
 export default function Home() {
   return (
-    <p>
+    <div>
       <TodoList />
-    </p>
+    </div>
     
   );
 }
