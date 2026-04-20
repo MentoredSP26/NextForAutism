d898e794449a620ccc7de9ef97c76b7cf4f0310d
bop111 <julian.yang0111@gmail.com>
2026-04-19 22:37:27 -0700

    added custom width and height

--- matchingplatform/src/components/ContainerBox/page.jsx
+++ matchingplatform/src/components/ContainerBox/page.jsx
@@ -1,13 +1,13 @@
 import styles from './styles.css'
 
-function ContainerBox({header, body}) {
+function ContainerBox({header, body, width, height}) {
 
     return (
-        <div className='wholeentirefullboxmegabooleanintfloatstrdoublelongsubwaychickenbaconranchmeltwithextraolives'>
+        <div className='wholeentirefullboxmegabooleanintfloatstrdoublelongsubwaychickenbaconranchmeltwithextraolives' style={{width:width, height:height}}>
             <div className='bluebackground'>
                 {header} 
             </div>
-            <div className='whitebackground'>
+            <div className='whitebackground' style={{height:height}}>
                 {body}
             </div>
         </div>
