3d819f48f123f53b7d7d741d579c93f27e917d31
christian <christiant_07@berkeley.edu>
2026-04-27 17:07:47 -0700

    Started working on some of the Established profile features

--- matchingplatform/src/app/page.js
+++ matchingplatform/src/app/page.js
@@ -3,12 +3,12 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import App from "./admin/page";
 import TodoList from "../components/TodoList/TodoList.jsx"
+import EstablishedProfile from "./established/profile.jsx";
 
 export default function Home() {
   return (
     <div>
-      <p>This is the home page (Next 4 Autism).</p>
-      <App />
+      <EstablishedProfile />
     </div>
     
   );
