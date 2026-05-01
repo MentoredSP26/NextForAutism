b624cb7d2add8c672eb22641a802527b1b493fb6
christian <christiant_07@berkeley.edu>
2026-04-30 22:33:35 -0700

    wire email buttons to edge functions from supabase

--- matchingplatform/src/app/page.js
+++ matchingplatform/src/app/page.js
@@ -3,12 +3,12 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import App from "./admin/page";
 import TodoList from "../components/TodoList/TodoList.jsx"
+import WeeklyMaterials from "../components/WeeklyMaterials/WeeklyMaterials.jsx";
 
 export default function Home() {
   return (
     <div>
-      <p>This is the home page (Next 4 Autism).</p>
-      <App />
+      <WeeklyMaterials />
     </div>
     
   );
