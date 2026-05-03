9d2452890e2d4fb8f0990964a1ecaabf29481d33
christian <christiant_07@berkeley.edu>
2026-05-03 15:32:04 -0700

    removed unnescesary styles.css file

--- matchingplatform/src/app/page.js
+++ matchingplatform/src/app/page.js
@@ -4,11 +4,12 @@ import styles from "./page.module.css";
 import App from "./admin/page";
 import TodoList from "../components/TodoList/TodoList.jsx"
 import WeeklyMaterials from "../components/WeeklyMaterials/WeeklyMaterials.jsx";
+import EstablishedDashboard from "./established/page.jsx";
 
 export default function Home() {
   return (
     <div>
-      <WeeklyMaterials />
+      <EstablishedDashboard />
     </div>
     
   );
